e6764a82f813a4978847e081651e989c4a7d38df
Artur Mostowski <artur.mostowski@currency-one.com>
2020-09-26 14:32:37 +0200

    bugfix: remove redundant spaces

---
+++ precache-manifest.3ceaaa06c472e140fbf418da2d0c10bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f66dbac4c1b558bff4ca",
+    "url": "/drools_generator/static/js/runtime~main.48684a76.js"
+  },
+  {
+    "revision": "444afc94bd84fc0ab614",
+    "url": "/drools_generator/static/js/main.85e4df0d.chunk.js"
+  },
+  {
+    "revision": "6c7625d27564d4430502",
+    "url": "/drools_generator/static/js/2.b60023d4.chunk.js"
+  },
+  {
+    "revision": "444afc94bd84fc0ab614",
+    "url": "/drools_generator/static/css/main.1dd8d6e7.chunk.css"
+  },
+  {
+    "revision": "3ea9da93bdfa3c9eae2a1d25719d2ccc",
+    "url": "/drools_generator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-editable-tree/precache-manifest.aff91ffccfb1b20150e62d7ca460866a.js"
+  "/drools_generator/precache-manifest.3ceaaa06c472e140fbf418da2d0c10bb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-editable-tree/index.html", {
+workbox.routing.registerNavigationRoute("/drools_generator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
